07b524aac1adedbc031741d485f13a4b5afb2d0a
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2023-06-28 20:54:02 +0200

    Completed function New-NuSpec

--- CICD/modules/Marc013.PwshUtils/Marc013.PwshUtils.psd1
+++ CICD/modules/Marc013.PwshUtils/Marc013.PwshUtils.psd1
@@ -34,4 +34,40 @@
 
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
     AliasesToExport      = @()
+
+    # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+    PrivateData          = @{
+
+        PSData = @{
+
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags                     = @(
+                'Pester',
+                'UnitTest'
+            )
+
+            # A URL to the license for this module.
+            LicenseUri               = 'https://opensource.org/license/mit/'
+
+            # A URL to the main website for this project.
+            ProjectUri               = 'https://github.com/Marc013/Marc013Stuff'
+
+            # A URL to an icon representing this module.
+            IconUri                  = 'None'
+
+            # ReleaseNotes of this module
+            ReleaseNotes             = 'Building something nice'
+
+            # Prerelease string of this module
+            # Prerelease = ''
+
+            # Flag to indicate whether the module requires explicit user acceptance for install/update/save
+            RequireLicenseAcceptance = $false
+
+            # External dependent modules of this module
+            # ExternalModuleDependencies = @()
+
+        } # End of PSData hashtable
+
+    } # End of PrivateData hashtable
 }
--- CICD/modules/Marc013.PwshUtils/Public/New-NuSpec.ps1
+++ CICD/modules/Marc013.PwshUtils/Public/New-NuSpec.ps1
@@ -2,17 +2,33 @@ function New-NuSpec {
     <#
     .SYNOPSIS
         A short one-line action-based description, e.g. 'Tests if a function is valid'
+
     .DESCRIPTION
-        A longer description of the function, its purpose, common use cases, etc.
+        New-NuSpec creates a NuGet package metadata file (.nuspec) of a PowerShell module.
+        For this the module manifest file (.psd1) is to contain at least the following information.
+
+        - RootModule
+        - ModuleVersion
+        - Author
+        - Description
+
+        Optional module manifest information (will be added to the NuGet metadata file when present):
+
+        - PrivateData.PSData.IconUri
+        - PrivateData.PSData.ProjectUri
+        - PrivateData.PSData.ReleaseNotes
+        - PrivateData.PSData.Tags
+
     .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
-    .LINK
-        Specify a URI to a help page, this will show when Get-Help -Online is used.
+        The blow URL might be helpful in choosing the appropriate license keyword,
+
+        https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository
+
     .EXAMPLE
         Test-MyTestFunction -Verbose
         Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
     #>
-    [CmdletBinding()]
+    [CmdletBinding(PositionalBinding = $false)]
     param (
         [Parameter(
             Mandatory = $true,
@@ -28,17 +44,18 @@ function New-NuSpec {
                 }
             }
         )]
-        [System.IO.FileInfo]$Path
+        [System.IO.FileInfo]$Path,
+        [Parameter(
+            Mandatory = $false,
+            HelpMessage = 'License keyword'
+        )]
+        [string]$License
     )
 
     $moduleManifest = Test-ModuleManifest -Path $Path -ErrorAction Stop
+    $requireLicenseAcceptance = $moduleManifest.PrivateData.PSData.RequireLicenseAcceptance.ToString().ToLower()
 
-    $outputPath = "$($path.DirectoryName)/$($moduleManifest.Name).test.nuspec"
-    $requireLicense = $false
-    $license = 'MIT'
-    $url = 'https://github.com/Marc013/Marc013Stuff'
-    # $releaseNotes = 'See the project for changes'
-    $tags = 'PowerShell', 'UnitTesting'
+    $outputPath = "$($Path.DirectoryName)/$($moduleManifest.Name).nuspec"
 
     $xmlSettings = New-Object System.Xml.XmlWriterSettings
     $xmlSettings.Indent = $true
@@ -46,20 +63,41 @@ function New-NuSpec {
     $xmlWriter = [System.XML.XmlWriter]::Create($outputPath, $xmlSettings)
     $xmlWriter.WriteStartElement('package')
     $xmlWriter.WriteStartElement('metadata')
-    $xmlWriter.WriteElementString('id', $($moduleManifest.Name))
-    $xmlWriter.WriteElementString('version', $($moduleManifest.Version))
-    $xmlWriter.WriteElementString('authors', $($moduleManifest.Author))
-    $xmlWriter.WriteElementString('requireLicenseAcceptance', $requireLicense.ToString().ToLower())
-    $xmlWriter.WriteStartElement('license')
-    $xmlWriter.WriteAttributeString('type', 'expression')
-    $xmlWriter.WriteString($license)
-    $xmlWriter.WriteEndElement()
-    # $xmlWriter.WriteElementString('icon', 'noClue')
-    $xmlWriter.WriteElementString('projectUrl', $url)
-    $xmlWriter.WriteElementString('description', $($moduleManifest.Description))
-    # $xmlWriter.WriteElementString('releaseNotes', $releaseNotes)
+    $xmlWriter.WriteElementString('id', $moduleManifest.Name)
+    $xmlWriter.WriteElementString('version', $moduleManifest.Version)
+    $xmlWriter.WriteElementString('authors', $moduleManifest.Author)
+
+    if ($requireLicenseAcceptance) {
+        $xmlWriter.WriteElementString('requireLicenseAcceptance', $requireLicenseAcceptance)
+    }
+
+    if ($license) {
+        $xmlWriter.WriteStartElement('license')
+        $xmlWriter.WriteAttributeString('type', 'expression')
+        $xmlWriter.WriteString($License)
+        $xmlWriter.WriteEndElement()
+    }
+
+    if ($moduleManifest.PrivateData.PSData.IconUri) {
+        $xmlWriter.WriteElementString('icon', $moduleManifest.PrivateData.PSData.IconUri)
+    }
+
+    if ($moduleManifest.ProjectUri.AbsoluteUri) {
+        $xmlWriter.WriteElementString('projectUrl', $moduleManifest.ProjectUri.AbsoluteUri)
+    }
+
+    $xmlWriter.WriteElementString('description', $moduleManifest.Description)
+
+    if ($moduleManifest.ReleaseNotes) {
+        $xmlWriter.WriteElementString('releaseNotes', $moduleManifest.ReleaseNotes)
+    }
+
     $xmlWriter.WriteElementString('copyright', '#{year}')
-    $xmlWriter.WriteElementString('tags', $($tags -join ' '))
+
+    if ($moduleManifest.Tags) {
+        $xmlWriter.WriteElementString('tags', $($moduleManifest.PrivateData.PSData.Tags -join ' '))
+    }
+
     $xmlWriter.WriteStartElement('dependencies')
     $xmlWriter.WriteStartElement('group')
     $xmlWriter.WriteAttributeString('targetFramework', '.NETStandard2.1')
